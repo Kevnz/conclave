4c9ff2c223ec84d08c51a3547db5388731ce720a
Kevin Isom <kevin.isom@gmail.com>
2013-09-20 08:04:34 +1200

    tidy

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var io = require('socket.io').listen(app.listen(app.get('port')));
 
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('message', { message: 'welcome to the chat' });
+    socket.emit('message', { message: 'welcome to the conclave' });
     socket.on('send', function (data) {
         io.sockets.emit('message', data);
     });
