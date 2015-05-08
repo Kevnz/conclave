429a9e3cf36ec2e1be271c2c75fde3da9d355e99
Kevin Isom <kevin.isom@gmail.com>
2015-05-08 12:50:43 +1200

    ES6 and updated express

--- app.js
+++ app.js
@@ -1,35 +1,108 @@
-var express = require('express'),
-    exphbs  = require('express3-handlebars'),
-    routes = require('./routes'),
-    http = require('http'),
-    path = require('path'),
-    app = express(),
-    expstate = require('express-state');
+'use strict';
 
-expstate.extend(app);
+import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import subdomain from 'express-subdomain';
+import routes from './routes/index';
+import users from './routes/users';
+import productApi from './routes/api/products';
+import cartAPI from './routes/api/cart';
+import passport from 'passport';
+import { OAuth2Strategy }  from 'passport-google-oauth';
+import xtconf from 'xtconf';
+import session from 'express-session';
+import processor from './auth/processor';
+import { get, put, del, post } from './resources/users';
+import { routeListing } from './utils/routes';
+import MongoSession from 'express-mongo-session';
 
-app.configure(function(){
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views'); 
-    app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-    app.set('view engine', 'handlebars');
+let conf = xtconf();
 
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(path.join(__dirname, 'public')));
+passport.serializeUser((user, done) => {
+  done(null, user);
 });
 
-app.configure('development', function(){
-    app.use(express.errorHandler());
-    app.locals.pretty = true;
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
 });
+passport.use(new OAuth2Strategy({
+    clientID: conf.get('google-key'),
+    clientSecret: conf.get('google-secret'),
+    callbackURL: 'http://localhost:4567/auth/google/return'
+  }, processor));
+var app = express();
 
-app.get('/', routes.index);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('query parser', 'extended');
+app.use(favicon(__dirname + '/public/favicons/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
 
+app.use(session({
+    secret: 'keyboard cat lives',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoSession()
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
  
+app.get('/auth/google', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/userinfo.email','https://www.googleapis.com/auth/userinfo.profile'] }));
+
+app.get('/auth/google/return', 
+  passport.authenticate('google', { successRedirect: '/',
+                                    failureRedirect: '/login' }));
+
+/// catch 404 and forwarding to error handler
+app.use((req, res, next) => {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+        next();
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+    next();
+});
+
+
+
+export default app;
+
 var io = require('socket.io').listen(app.listen(app.get('port')));
 
 
