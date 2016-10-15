a6fa5201f801d31182a324ceefa164ddc3dd0482
Kevin Isom <kevin.isom@gmail.com>
2016-10-15 23:12:01 +1300

    login and register routes

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,11 +1,37 @@
 /* eslint new-cap: 0 */
 const express = require('express');
-
+const User = require('../models/user');
 const router = express.Router();
 
 module.exports = function homeController() {
   router.get('/', (req, res) => {
     res.render('home/index', { title: 'Conclave' });
+  });
+  router.get('/login', (req, res) => {
+    res.render('home/login', { title: 'Conclave' });
+  });
+  router.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    User
+      .login(email, password)
+      .then((user) => {
+        res.redirect('/');
+      })
+      .catch((err) => {
+        res.redirect('/login');
+      });
+  });
+  router.get('/register', (req, res) => {
+    res.render('home/register', { title: 'Conclave' });
+  });
+  router.post('/register', (req, res, next) => {
+    const { firstName, lastName, email, password } = req.body;
+    User
+    .register(firstName, lastName, email, password)
+    .then((user) => {
+      res.redirect('/login');
+    })
+    .catch(err => next(err));
   });
   return router;
 };
