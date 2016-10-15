97208a1686979ac7af49e0e136d95cd12c485fab
Kevin Isom <kevin.isom@gmail.com>
2016-10-15 23:10:41 +1300

    user model and migration

---
+++ src/models/user.js
@@ -0,0 +1,29 @@
+const Promise = require('bluebird');
+const bcrypt = Promise.promisifyAll(require('bcrypt'));
+const bookshelf = require('./bookshelf');
+
+module.exports = bookshelf.model('User', {
+  tableName: 'users',
+  idAttribute: 'id'
+}, {
+  login: Promise.method(function authenticateUser(email, password) {
+    if (!email || !password) {
+      throw new Error('Email and password are both required');
+    }
+    return new this({ email: email.toLowerCase().trim() })
+      .fetch({ require: true })
+      .tap(function tapUser(user) {
+        return bcrypt
+          .compareAsync(password, user.get('password'))
+          .then(function compareResponse(res) {
+            if (!res) throw new Error('Invalid password');
+          });
+      });
+  }),
+  register: Promise.method(function registerUser(firstName, lastName, email, password) {
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    return new this({ firstName, lastName, email, password: hashedPassword }).save();
+  })
+});
