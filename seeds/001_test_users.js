62bf57a683b18caa0a8bc43e9b8c0745f12b70c3
Kevin Isom <kevin.isom@gmail.com>
2016-10-23 22:57:59 +1300

    seed data

--- seeds/001_test_users.js
+++ seeds/001_test_users.js
@@ -1,5 +1,7 @@
 const users = require('../fixtures/users-fixture.json').users;
 const topics = require('../fixtures/topics-fixture.json').topics;
+const messages1 = require('../fixtures/messages-fixture-1.json').messages;
+const messages2 = require('../fixtures/messages-fixture-2.json').messages;
 
 const mappedUsers = users.map((user) => {
   const fixed = {
@@ -11,10 +13,12 @@ const mappedUsers = users.map((user) => {
   return fixed;
 });
 
-exports.seed = function(knex) {
+exports.seed = function dataImport(knex) {
   return knex('users').del()
     .then(() => knex.batchInsert('users', mappedUsers))
     .then(() => knex.batchInsert('topics', topics))
+    .then(() => knex.batchInsert('messages', messages1))
+    .then(() => knex.batchInsert('messages', messages2))
     .then(() => {
       console.log('seed import done');
     });
