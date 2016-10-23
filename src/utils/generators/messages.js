aa27ee644e97f89b2b8915d9d6cc4756cb3421c6
Kevin Isom <kevin.isom@gmail.com>
2016-10-23 22:57:27 +1300

    generate message test data

---
+++ src/utils/generators/messages.js
@@ -0,0 +1,12 @@
+const faker = require('faker');
+const lorem = require('lorem-ipsum');
+
+module.exports = function messageGen(topic = 1, createdBy = 1, parent = null) {
+  return {
+    title: faker.company.catchPhrase(),
+    body: lorem({ count: 40, units: 'words' }),
+    created_by: createdBy,
+    topic_id: topic,
+    parent_id: parent
+  };
+};
