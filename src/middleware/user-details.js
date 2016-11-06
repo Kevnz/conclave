770fbbd7bc1a397ff83312cdf47aded932ceda91
Kevin Isom <kevin.isom@gmail.com>
2016-11-06 15:42:05 +1300

    get user details middleware

---
+++ src/middleware/user-details.js
@@ -0,0 +1,16 @@
+const User = require('../models/user');
+
+module.exports = function(req, res, next) {
+  console.log('user details middleware');
+  new User({id: req.user.id})
+    .fetch()
+    .then((user) => {
+      console.log('user', user.toJSON());
+      res.locals.user = user.toJSON();
+      next();
+    })
+    .catch((err) => {
+      console.log('err', err);
+      next(err);
+    });
+};
