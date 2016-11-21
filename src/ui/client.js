4199f72c7d73e186603746809297343fdcc08a58
Kevin Isom <kevinisom@Shenandoah.fritz.box>
2016-11-21 21:35:13 +1300

    react ui start

---
+++ src/ui/client.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MainApp from './index';
+
+console.log('window state', window.__APP_STATE__);
+const state = window.__APP_STATE__;
+
+ReactDOM.render(
+  <MainApp topics={state.topics} />,
+  document.getElementById('app')
+);
