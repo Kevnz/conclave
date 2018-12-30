c1bef46f4eabd73581c06e7e684d1325ebd6c9c5
Kevin Isom <kevin.isom@gmail.com>
2018-12-30 17:12:12 +1300

    Add other types

--- src/server/graphql/types.js
+++ src/server/graphql/types.js
@@ -7,16 +7,33 @@ const typeDefs = gql`
     email: String
     token: String
   }
+  type Topic {
+    title: String
+    description: String
+    createdBy: User
+    parent: Topic
+    messages: [Message]
+  }
+  type Message {
+    title: String
+    body: String
+    createdBy: User
+    parent: Topic
+    replies: [Message]
+  }
   type Query {
+    topics(): [Topic]
+    messages(topicId: ID!): [Message]
     user(token: ID): User
     login(email: String!, password: String!): User!
   }
 
   type Mutations {
-    signup(firstName: String!
-    lastName: String!
-    email: String!
-    password: String!
+    signup(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
     ): User!
   }
 `
