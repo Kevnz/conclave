600d1adc5be981c6b4656edfb81dc664c40f66ba
Kevin Isom <kevin.isom@gmail.com>
2019-03-10 13:16:04 +1300

    update storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,10 @@
-const webpack = require('webpack')
+// you can use this file to add your custom webpack plugins, loaders and anything you like.
+// This is just the basic way to add additional webpack configurations.
+// For more information refer the docs: https://storybook.js.org/configurations/custom-webpack-config
 
-const baseConfig = require('../src/webpack/config')
-
-const devConfig = {
-  plugins: [
-
-  ],
-}
-
-module.exports = {
-  ...baseConfig,
-  ...devConfig,
-}
+// IMPORTANT
+// When you add this file, we won't add the default configurations which is similar
+// to "React Create App". This only has babel loader to load JavaScript.
 
 module.exports = {
   plugins: [
