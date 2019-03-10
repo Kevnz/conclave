bc68c01be7bcc48f487518f75e2d693da1adf137
Kevin Isom <kevin.isom@gmail.com>
2019-03-10 13:27:07 +1300

    put the helmet on

--- src/ui/features/contact.js
+++ src/ui/features/contact.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import { Form, TextArea, TextBox } from 'react-form-elements'
 const ContactForm = () => {
-
   return (
     <main>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Conclave - Contact Us</title>
+      </Helmet>
       <section>
         <h2>Contact</h2>
         <div>
@@ -14,11 +18,7 @@ const ContactForm = () => {
               console.log('Message', values.message)
             }}
           >
-            <TextBox
-              name="userName"
-              label="Your Name"
-              initialValue=""
-            />
+            <TextBox name="userName" label="Your Name" initialValue="" />
             <TextBox
               type="email"
               name="userEmail"
