034c47cec8f4c8fae5bab8499f403a25a9c49839
Kevin Isom <kevin.isom@gmail.com>
2019-03-10 13:28:03 +1300

    remove the mini css import

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'mini.css'
 import App from './core/app'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-module.hot.accept()
