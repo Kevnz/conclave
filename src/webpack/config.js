62b0a1b5c17272b12266f4a125a7f81b7d0a840e
Kevin Isom <kevin.isom@gmail.com>
2019-03-10 13:18:02 +1300

    config changes

--- src/webpack/config.js
+++ src/webpack/config.js
@@ -32,7 +32,7 @@ module.exports = {
     },
   },
   output: {
-    path: path.join(__dirname, '/dist'),
+    path: path.join(__dirname, '/public'),
     publicPath: '/',
     filename: 'bundle.js',
   },
--- src/webpack/dev.config.js
+++ src/webpack/dev.config.js
@@ -1,24 +1,28 @@
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+//  .BundleAnalyzerPlugin
 const baseConfig = require('./config')
 
 const devConfig = {
+  entry: './src/ui/index.js',
   plugins: [
+    /*
     new BundleAnalyzerPlugin({
       analyzerMode: 'static',
     }),
-    new webpack.HotModuleReplacementPlugin(),
+    */
     new HtmlWebpackPlugin({
       title: 'Custom template',
       template: './src/ui/index.html',
       historyApiFallback: true,
     }),
   ],
+  devtool: 'cheap-module-source-map',
   devServer: {
-    contentBase: './dist',
-    hot: true,
+    contentBase: './public',
+    hot: false,
+    historyApiFallback: true,
     proxy: {
       '/graphql': 'http://localhost:4567/',
     },
