cdcfc509bfdf53ac62d1ff8fd128330d777afb76
Kevin Isom <kevin.isom@gmail.com>
2019-03-10 13:29:36 +1300

    setup for hooks, auth, and error boundry

--- src/ui/core/app.js
+++ src/ui/core/app.js
@@ -1,28 +1,39 @@
-import React, { Component, Fragment } from 'react'
-import { ApolloProvider } from 'react-apollo'
+import React from 'react'
+import { ApolloProvider } from 'react-apollo-hooks'
 import { Router } from '@reach/router'
 import client from './client'
+import { Layout } from './layout'
+import { AuthProvider } from './context/auth'
+import { ErrorBoundary } from './boundry'
 
 const About = React.lazy(() => import('../features/about'))
 const Home = React.lazy(() => import('../features/home'))
 const Contact = React.lazy(() => import('../features/contact'))
 const Register = React.lazy(() => import('../features/register'))
-export default class App extends Component {
-  render() {
-    return (
-      <ApolloProvider client={client}>
-        <Fragment>
-          <h1>Conclave</h1>
-          <React.Suspense fallback={<div>Loading</div>}>
-            <Router>
-              <Home path="/" />
-              <About path="/about" />
-              <Contact path="/contact" />
-              <Register path="/register" />
-            </Router>
-          </React.Suspense>
-        </Fragment>
-      </ApolloProvider>
-    )
-  }
+const Login = React.lazy(() => import('../features/login'))
+
+export const App = () => {
+  return (
+    <ApolloProvider client={client}>
+      <ErrorBoundary>
+        <AuthProvider>
+          <Layout>
+            <ErrorBoundary>
+              <React.Suspense fallback={<div>Loading</div>}>
+                <Router>
+                  <Home path="/" />
+                  <About path="/about" />
+                  <Contact path="/contact" />
+                  <Register path="/register" />
+                  <Login path="/login" />
+                </Router>
+              </React.Suspense>
+            </ErrorBoundary>
+          </Layout>
+        </AuthProvider>
+      </ErrorBoundary>
+    </ApolloProvider>
+  )
 }
+
+export default App
