e7837a22ed27c6b333917b481205f3ea4933a4fa
Kevin Isom <kevin.isom@gmail.com>
2019-07-29 10:38:58 +1200

    Bring brightleaf elements to the party now

--- src/ui/core/layout.js
+++ src/ui/core/layout.js
@@ -1,67 +1,240 @@
 import React, { useContext } from 'react'
 import { navigate } from '@reach/router'
-import withStyles from 'react-jss'
+import {
+  BaseIcon,
+  Box,
+  Button,
+  Column,
+  Columns,
+  Container,
+  Content,
+  Control,
+  Field,
+  Footer,
+  Menu,
+  MenuLabel,
+  MenuList,
+  MenuListItem,
+  NavBar,
+  NavBarStart,
+  NavBarEnd,
+  NavBarMenu,
+  NavBarBrand,
+  NavBarItem,
+  Icon,
+  Tag,
+  Tags,
+} from '@brightleaf/elements'
+import { useStyleSheet } from '@brightleaf/react-hooks/lib/use-stylesheet'
 import { AccountLink } from '../components/account-link'
-import { appStyles } from '../styles/app'
 import { AuthContext } from './context/auth'
+import './forum.css'
 
-const BaseLayout = ({ children, classes }) => {
+export const Layout = ({ children }) => {
+  useStyleSheet('https://fonts.googleapis.com/css?family=Open+Sans:300,400,700')
   const { state, dispatch } = useContext(AuthContext)
   return (
-    <main className={classes.appBase}>
-      <header>
-        <h1>The Conclave</h1>
-        <nav>
-          <ul>
-            <li>
-              {state.isLoggedIn ? (
-                <>
-                  <AccountLink
-                    name={`${state.user.firstName} ${state.user.lastName}`}
+    <>
+      <NavBar className="navbar is-white topNav">
+        <Container className="container">
+          <NavBarBrand src="brightleafjs-logo.png" width="112" height="28" />
+          <NavBarMenu id="topNav" className="navbar-menu">
+            <NavBarStart>
+              <NavBarItem>
+                <a href="/">Home</a>
+              </NavBarItem>
+              <NavBarItem>
+                <a href="#LANDING">Landing</a>
+              </NavBarItem>
+              <NavBarItem>
+                <a href="#BLOG">Blog</a>
+              </NavBarItem>
+            </NavBarStart>
+            <NavBarEnd>
+              <NavBarItem>
+                <div>
+                  <Field isGrouped>
+                    {state.isLoggedIn ? (
+                      <>
+                        <Control>
+                          <AccountLink
+                            name={`${state.user.firstName} ${state.user.lastName}`}
+                          />
+                        </Control>
+                        <Control>
+                          <Button
+                            isSmall
+                            isInfo
+                            isOutlined
+                            onClick={e => {
+                              e.preventDefault()
+                              dispatch({ type: 'logout' })
+                            }}
+                          >
+                            <Icon icon="sign-out-alt" fas />
+                            <span>Logout</span>
+                          </Button>
+                        </Control>
+                      </>
+                    ) : (
+                      <>
+                        <Control>
+                          <Button
+                            className="button is-small "
+                            onClick={e => {
+                              e.preventDefault()
+                              navigate('/login')
+                            }}
+                          >
+                            <Icon icon="user" fas />
+                            <span>Login</span>
+                          </Button>
+                        </Control>
+                        <Control>
+                          <Button
+                            isSmall
+                            isAnchor
+                            onClick={e => {
+                              e.preventDefault()
+                              navigate('/register')
+                            }}
+                          >
+                            <Icon icon="user-plus" fas />
+                            <span>Register</span>
+                          </Button>
+                        </Control>
+                      </>
+                    )}
+                  </Field>
+                </div>
+              </NavBarItem>
+            </NavBarEnd>
+          </NavBarMenu>
+        </Container>
+      </NavBar>
+      <NavBar isWhite>
+        <Container>
+          <NavBarMenu>
+            <NavBarStart>
+              <NavBarItem isActive>
+                <a href="#">Popular</a>
+              </NavBarItem>
+              <NavBarItem>
+                <a href="#">Recent</a>
+              </NavBarItem>
+              <NavBarItem>
+                <a href="#">Rising</a>
+              </NavBarItem>
+            </NavBarStart>
+            <NavBarEnd>
+              <NavBarItem>
+                <div>
+                  <input
+                    className="input"
+                    type="search"
+                    placeholder="Search forum..."
                   />
-                  <a
-                    href=""
-                    onClick={e => {
-                      e.preventDefault()
-                      dispatch({ type: 'logout' })
-                    }}
-                  >
-                    Logout
-                  </a>
-                </>
-              ) : (
-                <>
-                  <a
-                    href="#"
-                    onClick={e => {
-                      e.preventDefault()
-                      navigate('/login')
-                    }}
-                  >
-                    Login
-                  </a>{' '}
-                  {' | '}
-                  <a
-                    href="#"
-                    onClick={e => {
-                      e.preventDefault()
-                      navigate('/register')
-                    }}
-                  >
-                    Register
-                  </a>
-                </>
-              )}
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <section>
-        <div>{children}</div>
-      </section>
-    </main>
+                </div>
+              </NavBarItem>
+            </NavBarEnd>
+          </NavBarMenu>
+        </Container>
+      </NavBar>
+      <Container>
+        <Columns>
+          <Column is="3">
+            <Button
+              isAnchor
+              isPrimary
+              isLarge
+              className="is-block is-alt"
+              href="#"
+            >
+              New Post
+            </Button>
+            <Menu className="menu">
+              <MenuLabel>Tags</MenuLabel>
+              <MenuList>
+                <MenuListItem>
+                  <Tag isPrimary isMedium>
+                    Dashboard
+                  </Tag>
+                </MenuListItem>
+                <MenuListItem>
+                  <Tag className="is-link" isMedium>
+                    Customers
+                  </Tag>
+                </MenuListItem>
+                <MenuListItem>
+                  <Tag isLight isDanger isMedium>
+                    Authentication
+                  </Tag>
+                </MenuListItem>
+                <MenuListItem>
+                  <Tag isDark isMedium>
+                    Payments
+                  </Tag>
+                </MenuListItem>
+                <MenuListItem>
+                  <Tag isSuccess isMedium>
+                    Transfers
+                  </Tag>
+                </MenuListItem>
+                <MenuListItem>
+                  <Tag isWarning isMedium>
+                    Balance
+                  </Tag>
+                </MenuListItem>
+                <MenuListItem>
+                  <Tag isMedium>Question</Tag>
+                </MenuListItem>
+              </MenuList>
+            </Menu>
+          </Column>
+          <Column is="9">
+            <Box>{children}</Box>
+          </Column>
+        </Columns>
+      </Container>
+      <Footer>
+        <Container>
+          <Content hasTextCentered>
+            <Columns isMobile isCentered>
+              <Field isGrouped isGroupedMultiline>
+                <Control>
+                  <Tags hasAddons>
+                    <Tag
+                      isLink
+                      href="https://github.com/dansup/bulma-templates"
+                    >
+                      Bulma Templates
+                    </Tag>
+                    <Tag isLight>Daniel Supernault</Tag>
+                  </Tags>
+                </Control>
+                <Control>
+                  <Tags hasAddons>
+                    <Tag isLink href="https://github.com/brightleaf/elements">
+                      Brightleaf/Elements
+                    </Tag>
+                    <Tag isLight>Kevin Isom</Tag>
+                  </Tags>
+                </Control>
+                <Control className="control">
+                  <Tags hasAddons className="tags has-addons">
+                    <Tag className="is-link">The source code is licensed</Tag>
+                    <Tag isLight>
+                      MIT &nbsp;<BaseIcon fab icon="github"></BaseIcon>
+                    </Tag>
+                  </Tags>
+                </Control>
+              </Field>
+            </Columns>
+          </Content>
+        </Container>
+      </Footer>
+    </>
   )
 }
-export const Layout = withStyles(appStyles)(BaseLayout)
 
 export default Layout
