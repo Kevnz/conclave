33d2ae9c558ec569dd83f926f1f10da82110c0d6
Kevin Isom <kevin.isom@gmail.com>
2019-07-29 10:46:07 +1200

    context import

--- src/ui/features/add-topic.js
+++ src/ui/features/add-topic.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import gql from 'graphql-tag'
 import { useMutation } from 'react-apollo-hooks'
