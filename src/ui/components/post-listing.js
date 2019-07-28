3a4f9aff20ab7480e7120ac2d4bff3708ddd3c19
Kevin Isom <kevin.isom@gmail.com>
2019-07-29 10:41:37 +1200

    Post listing for new style

---
+++ src/ui/components/post-listing.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import {
+  BaseIcon,
+  Content,
+  Image,
+  MediaContent,
+  MediaLeft,
+  MediaObject,
+  MediaRight,
+  Tag,
+} from '@brightleaf/elements'
+import { timeAgo } from '../utils/time-ago'
+/*
+  title: String
+  body: String
+  createdBy: User
+
+  //topic
+    id: Int
+  title: String
+  description: String
+  createdBy: User
+  parent: Topic
+  childTopics: [Topic]
+  messages: [Message]
+  */
+export default function PostListing({
+  title,
+  body,
+  createdBy,
+  tag,
+  createdOn,
+  parent,
+}) {
+  return (
+    <article className="post">
+      <h4>{title}</h4>
+      <MediaObject as="div">
+        <MediaLeft as="div">
+          <Image
+            as="p"
+            is32
+            src="http://bulma.io/images/placeholders/128x128.png"
+          />
+        </MediaLeft>
+        <MediaContent>
+          <Content>
+            <p>
+              <a href="#">{createdBy.username}</a> replied {timeAgo(createdOn)}{' '}
+              &nbsp;
+              <Tag as="span">{tag}</Tag>
+            </p>
+          </Content>
+        </MediaContent>
+        <MediaRight>
+          <span className="has-text-grey-light">
+            <BaseIcon icon="comments" fa /> 13
+          </span>
+        </MediaRight>
+      </MediaObject>
+    </article>
+  )
+}
