81b183ca59c3084e482a8e6470f68888dceaafa3
Kevin Isom <kevin.isom@gmail.com>
2019-07-30 11:39:48 +1200

    Add a second loader

--- src/ui/components/loader.js
+++ src/ui/components/loader.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import './loader.scss'
+
 export const Loader = () => {
   return (
     <div className="prepreloader">
       <div className="preloader">
         <div className="loader"></div>
+      </div>
+    </div>
+  )
+}
+
+export const GridLoader = () => {
+  return (
+    <div className="prepreloader">
+      <div className="preloader">
+        <div className="lds-grid">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   )
