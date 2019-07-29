42941fbe3ec03080483c2adda9163f0663388cd7
Kevin Isom <kevin.isom@gmail.com>
2019-07-29 13:57:10 +1200

    topic add ui

--- src/ui/components/add-topic.js
+++ src/ui/components/add-topic.js
@@ -5,9 +5,21 @@ import { Form, TextBox, TextArea, Button } from 'react-form-elements'
 export const AddTopic = ({ onSubmit }) => {
   return (
     <div>
-      <Form onSubmit={onSubmit}>
-        <TextBox name="title" label="Title" />
-        <TextArea name="description" label="Description" />
+      <Form onSubmit={onSubmit} name="add-topic">
+        <TextBox
+          name="title"
+          label="Title"
+          className="field control"
+          labelClassName="label"
+          inputClassName="input"
+        />
+        <TextArea
+          name="description"
+          label="Description"
+          className="field control"
+          labelClassName="label"
+          inputClassName="input"
+        />
         <Button>Save</Button>
       </Form>
     </div>
