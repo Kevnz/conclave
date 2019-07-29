8d5d7c9de23bcd42ca43899bb97be721eb905fec
Kevin Isom <kevin.isom@gmail.com>
2019-07-30 11:38:16 +1200

    if id render link

--- src/server/views/helpers/row.js
+++ src/server/views/helpers/row.js
@@ -1,5 +1,8 @@
 module.exports = (item, attributes) => {
   const elements = attributes.map(attr => {
+    if (attr === 'id') {
+      return `<td> <a href="${item[attr]}">${item[attr]}</a> </td>`
+    }
     return `<td> ${item[attr]} </td>`
   })
 
