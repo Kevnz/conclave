90d052de971ee162a2d7a3cf43f52eaca4bf5e3f
Kevin Isom <kevin.isom@gmail.com>
2019-07-29 13:57:32 +1200

    No apollo

--- src/ui/core/app.js
+++ src/ui/core/app.js
@@ -12,28 +12,31 @@ const Home = React.lazy(() => import('../pages/home'))
 const Contact = React.lazy(() => import('../pages/contact'))
 const Register = React.lazy(() => import('../pages/register'))
 const Login = React.lazy(() => import('../pages/login'))
-
+const AddTopic = React.lazy(() => import('../features/add-topic'))
+const AddPost = React.lazy(() => import('../features/add-post'))
+const Topic = React.lazy(() => import('../pages/topic'))
 export const App = () => {
   return (
-    <ApolloProvider client={client}>
-      <ErrorBoundary>
-        <AuthProvider>
-          <Layout>
-            <ErrorBoundary>
-              <React.Suspense fallback={<div>Loading</div>}>
-                <Router>
-                  <Home path="/" />
-                  <About path="/about" />
-                  <Contact path="/contact" />
-                  <Register path="/register" />
-                  <Login path="/login" />
-                </Router>
-              </React.Suspense>
-            </ErrorBoundary>
-          </Layout>
-        </AuthProvider>
-      </ErrorBoundary>
-    </ApolloProvider>
+    <ErrorBoundary>
+      <AuthProvider>
+        <Layout>
+          <ErrorBoundary>
+            <React.Suspense fallback={<div>Loading</div>}>
+              <Router>
+                <Home path="/" />
+                <About path="/about" />
+                <Contact path="/contact" />
+                <Register path="/register" />
+                <Login path="/login" />
+                <Topic path="/topic/:topicId" />
+                <AddTopic path="/topic/add" />
+                <AddPost path="/topic/:topicId/post" />
+              </Router>
+            </React.Suspense>
+          </ErrorBoundary>
+        </Layout>
+      </AuthProvider>
+    </ErrorBoundary>
   )
 }
 
