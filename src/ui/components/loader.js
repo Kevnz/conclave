8f744a035b69a47281137056ffe5e5e089652fa1
Kevin Isom <kevin.isom@gmail.com>
2019-07-29 21:09:01 +1200

    Loader component

---
+++ src/ui/components/loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './loader.scss'
+export const Loader = () => {
+  return (
+    <div className="prepreloader">
+      <div className="preloader">
+        <div className="loader"></div>
+      </div>
+    </div>
+  )
+}
