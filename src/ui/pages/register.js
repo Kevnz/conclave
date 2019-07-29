08b066ad8c3aa461b2dd08cd3a0d0ca6da5a9d03
Kevin Isom <kevin.isom@gmail.com>
2019-07-29 13:59:40 +1200

    update to brightleaf react-hooks

--- src/ui/pages/register.js
+++ src/ui/pages/register.js
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react'
 import { Form, TextBox, EmailInput, Button } from 'react-form-elements'
-import gql from 'graphql-tag'
-import { useMutation } from 'react-apollo-hooks'
+
+import { useMutation } from '@brightleaf/react-hooks/lib/use-mutation'
 import { navigate } from '@reach/router'
 import { AuthContext } from '../core/context/auth'
 
-const REGISTER_MUTATION = gql`
+const REGISTER_MUTATION = `
   mutation Signup($newUserInput: NewUserInput!) {
     signup(newUserInput: $newUserInput) {
       token
@@ -21,37 +21,42 @@ const REGISTER_MUTATION = gql`
 `
 
 const RegisterForm = () => {
-  const registerUser = useMutation(REGISTER_MUTATION)
+  const { error, loading, makeQuery, data } = useMutation(
+    '/graphql',
+    REGISTER_MUTATION
+  )
   const { state, dispatch } = useContext(AuthContext)
 
+  if (data && data.signup) {
+    const { token, user } = data.signup
+
+    dispatch({
+      type: 'login',
+      payload: { token, user },
+    })
+    navigate('/')
+  }
   if (state.isLoggedIn) {
     return <div>Account already exists</div>
   }
   return (
     <main>
+      {error && <div>Error</div>}
+      {loading && <div>Loading</div>}
       <section>
         <h2>Create your account</h2>
         <div>
           <Form
             name="registerForm"
-            onSubmit={({
-              firstName, lastName, email, password, username
-            }) => {
-              registerUser({
-                update: (proxy, mutationResult) => {
-                  console.info('prox', proxy)
-                  console.info('mutie', mutationResult)
-                  const { token, user } = mutationResult.data.signup
-
-                  dispatch({
-                    type: 'login',
-                    payload: { token, user },
-                  })
-                  navigate('/')
+            onSubmit={({ firstName, lastName, email, password, username }) => {
+              makeQuery({
+                newUserInput: {
+                  firstName,
+                  lastName,
+                  email,
+                  password,
+                  username,
                 },
-                variables: { newUserInput: {
-                  firstName, lastName, email, password, username
-                } },
               })
             }}
           >
