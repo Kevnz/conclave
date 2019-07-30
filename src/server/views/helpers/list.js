0a9aeaccc688e918f054898be2df3dca4158424c
Kevin Isom <kevin.isom@gmail.com>
2019-07-30 17:11:05 +1200

    classes and current route

--- src/server/views/helpers/list.js
+++ src/server/views/helpers/list.js
@@ -1,13 +1,17 @@
-module.exports = items => {
+module.exports = (items, className, current) => {
+  console.log('current', current)
+  console.log(items)
   const rows =
     items && items.length > 0
       ? items.map(
           item =>
-            `<li><a class="model-name" href="/admin/view/${item}/">${item}</a></li>`
+            `<li><a class="model-name ${
+              item === current ? 'is-active' : ''
+            }" href="/admin/view/${item}/">${item}</a></li>`
         )
       : [` `]
 
-  return `<ul>
+  return `<ul class="${className}">
   ${rows.join('')}
   </ul>`
 }
