95bd7d2ad7240195243c01af76efb3423ef4cf04
Kevin Isom <kevin.isom@gmail.com>
2019-07-30 17:11:15 +1200

    format dates

--- src/server/views/helpers/row.js
+++ src/server/views/helpers/row.js
@@ -1,10 +1,18 @@
+const timeAgo = require('../../utils/time-ago')
 module.exports = (item, attributes) => {
   const elements = attributes.map(attr => {
     if (attr === 'id') {
       return `<td> <a href="${item[attr]}">${item[attr]}</a> </td>`
     }
+    if (
+      attr === 'created_at' ||
+      attr === 'updated_at' ||
+      attr === 'createdOn'
+    ) {
+      return `<td> ${timeAgo(item[attr])} </td>`
+    }
+
     return `<td> ${item[attr]} </td>`
   })
-
   return `<tr>${elements.join('')}</tr>`
 }
