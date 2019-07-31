ab47c51a54c8ca835b700199bf8a56506302b046
Kevin Isom <kevin.isom@gmail.com>
2019-07-31 13:09:37 +1200

    handle more items

--- src/server/views/helpers/form.js
+++ src/server/views/helpers/form.js
@@ -1,6 +1,21 @@
 const to = require('to-case')
+const readonly = ['created_at', 'createdOn', 'updated_at']
 module.exports = (attributes, item = null) => {
   const rows = attributes.map(attr => {
+    const isDisabled = readonly.includes(attr)
+
+    if (!item) {
+      return `<div class="field">
+        <label class="label">${to.capital(attr)}</label>
+        <div class="control">
+          <input class="input" ${isDisabled ? 'disabled' : ''}
+            type="text"
+            name="${attr}"
+            value=""
+          />
+        </div>
+      </div>`
+    }
     if (attr === '_id' && item) {
       return `<div class="field">
       <label class="label">${to.capital(attr)}</label><div class="control">
@@ -14,23 +29,31 @@ module.exports = (attributes, item = null) => {
     } else if (attr === 'user') {
       return ''
     }
+
     if (item[attr] && item[attr].length) {
       console.log(item[attr].length)
     }
     if (item[attr] && item[attr].length > 100) {
       return `<div class="field">
-      <label class="label">${to.capital(attr)}</label>
-      <div class="control">
-      <textarea class="textarea" name="${attr}">${item[attr]}</textarea>
-      </div> </div>`
+        <label class="label">${to.capital(attr)}</label>
+        <div class="control">
+          <textarea class="textarea"
+            name="${attr}">${item[attr]}</textarea>
+        </div>
+      </div>`
     }
+
     return `<div class="field">
-      <label class="label">${to.capital(attr)}</label>
-      <div class="control">
-      <input class="input" type="text" name="${attr}" value="${
-      item ? item[attr] : ''
-    }" />
-      </div> </div>`
+        <label class="label">${to.capital(attr)}</label>
+        <div class="control">
+          <input class="input"
+            ${isDisabled ? 'disabled' : ''}
+            type="text"
+            name="${attr}"
+            value="${item ? item[attr] : ''}"
+          />
+        </div>
+      </div>`
   })
 
   return `<form class="form" method="${item ? 'PUT' : 'POST'}">
