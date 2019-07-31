735283907d8b607b3a3bc48931fa7642c0a5bf9d
Kevin Isom <kevin.isom@gmail.com>
2019-08-01 09:23:26 +1200

    mini

--- src/webpack/prod.config.js
+++ src/webpack/prod.config.js
@@ -1,10 +1,16 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const baseConfig = require('./config')
 const prodConfig = {
   plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
     new HtmlWebpackPlugin({
       title: 'Custom template',
       template: './src/ui/index.html',
