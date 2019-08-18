55eb37cc59619e5b5cfd8b489e1dabf51681075f
Kevin Isom <kevin.isom@gmail.com>
2019-08-18 23:09:01 +1200

    update styles

--- src/ui/pages/register.js
+++ src/ui/pages/register.js
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { Form, TextBox, EmailInput, Button } from 'react-form-elements'
-
+import { Control } from '@brightleaf/elements'
 import { useMutation } from '@brightleaf/react-hooks/lib/use-mutation'
 import { navigate } from '@reach/router'
 import { AuthContext } from '../core/context/auth'
@@ -26,7 +26,7 @@ const RegisterForm = () => {
     REGISTER_MUTATION
   )
   const { state, dispatch } = useContext(AuthContext)
-
+  console.log({ error, loading, makeQuery, data })
   if (data && data.signup) {
     const { token, user } = data.signup
 
@@ -48,6 +48,7 @@ const RegisterForm = () => {
         <div>
           <Form
             name="registerForm"
+            className="form"
             onSubmit={({ firstName, lastName, email, password, username }) => {
               makeQuery({
                 newUserInput: {
@@ -60,28 +61,60 @@ const RegisterForm = () => {
               })
             }}
           >
-            <TextBox name="firstName" label="First Name" initialValue="" />
-            <TextBox name="lastName" label="First Name" initialValue="" />
+            <TextBox
+              name="firstName"
+              label="First Name"
+              initialValue=""
+              className="field control"
+              labelClassName="label"
+              inputClassName="input"
+            />
+            <TextBox
+              name="lastName"
+              label="Last Name"
+              initialValue=""
+              className="field control"
+              labelClassName="label"
+              inputClassName="input"
+            />
             <EmailInput
               type="email"
               name="email"
-              label="Your Email"
+              label="Email"
               initialValue=""
+              className="field control"
+              labelClassName="label"
+              inputClassName="input"
             />
-            <TextBox name="username" label="Username" initialValue="" />
+            <TextBox
+              name="username"
+              label="Username"
+              initialValue=""
+              className="field control"
+              labelClassName="label"
+              inputClassName="input"
+            />
             <TextBox
               name="password"
               label="Password"
               initialValue=""
               type="password"
+              className="field control"
+              labelClassName="label"
+              inputClassName="input"
             />
             <TextBox
               name="verifyPassword"
               label="Verify Password"
               initialValue=""
               type="password"
+              className="field control"
+              labelClassName="label"
+              inputClassName="input"
             />
-            <Button>Register</Button>
+            <Control>
+              <Button className="button is-primary">Register</Button>
+            </Control>
           </Form>
         </div>
       </section>
