2fb16896879b001d123c19fbedb20c2a24b6cd9c
Kevin Isom <kevin.isom@gmail.com>
2019-08-18 23:34:43 +1200

    Login styles

--- src/ui/components/login.js
+++ src/ui/components/login.js
@@ -6,8 +6,21 @@ export const LoginInput = ({ onSubmit }) => {
   return (
     <div>
       <Form onSubmit={onSubmit}>
-        <EmailInput name="email" label="Email" />
-        <TextBox name="password" label="Password" type="password" />
+        <EmailInput
+          name="email"
+          label="Email"
+          className="field control"
+          labelClassName="label"
+          inputClassName="input"
+        />
+        <TextBox
+          name="password"
+          label="Password"
+          type="password"
+          className="field control"
+          labelClassName="label"
+          inputClassName="input"
+        />
         <Button>Login</Button>
       </Form>
     </div>
