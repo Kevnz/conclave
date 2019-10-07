f16e2fc0343578ffd345cbc4906d1456e42b04e3
Kevin Isom <kevin.isom@gmail.com>
2019-10-07 22:33:07 +1300

    tags

--- src/utils/generators/messages.js
+++ src/utils/generators/messages.js
@@ -1,5 +1,26 @@
 const faker = require('faker')
 
+const random = (min, max) => Math.round(Math.random() * (max - min) + min)
+
+const Tags = [
+  'JavaScript',
+  'CSS',
+  'HTML',
+  'Dashboard',
+  'Web',
+  'Server',
+  'Performance',
+]
+
+const getTags = () => {
+  const tags = []
+  const total = random(0, Tags.length)
+  for (let index = 0; index < total; index++) {
+    tags.push(Tags[index])
+  }
+  return tags
+}
+
 module.exports = (id, topic = 1, createdBy = 1, parent = null) => ({
   id: id,
   title: faker.company.catchPhrase(),
@@ -7,4 +28,5 @@ module.exports = (id, topic = 1, createdBy = 1, parent = null) => ({
   created_by: createdBy,
   topic_id: topic,
   parent_id: parent,
+  tags: getTags(),
 })
